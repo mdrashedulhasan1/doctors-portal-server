68dd44954dbaa97e4546f51774dfdf153e3036e2
unknown <rashedul15-1718@diu.edu.bd>
2022-09-10 22:17:55 -0700

    Server Update

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express')
+require("dotenv").config();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
+app.use(cors())
+app.use(express.json())
 
-
-const uri = "mongodb+srv://doctor_admin:N05DzdjLvAIhp0BT@cluster0.thykviz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.thykviz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    const serviceCollection = client.db("doctors_portal").collection("services");
+    app.get('/service', async(req, res) => {
+      const query = { };
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use(cors())
 app.use(express.json())
